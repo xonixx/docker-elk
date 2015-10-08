63338f1eab018218129634f97c474062a8a95520
xonix <xonixx@gmail.com>
2015-10-08 17:41:36 +0300

    es data in volume: preserve es data after container rm

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-sudo docker run -d --name elk -p 81:80 -p 9200:9200 -p 12345:12345 xonixx/elk
+mydir=$(dirname $0)
+mydir=$(cd $mydir; pwd)
+
+sudo docker run -d --name elk \
+ -v $mydir/es_data:/usr/share/elasticsearch/data \
+ -p 81:80 \
+ -p 9200:9200 \
+ -p 12345:12345 \
+ xonixx/elk
