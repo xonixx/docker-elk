44387302a291af174795de24b2d56fcb88e99355
xonix <xonixx@gmail.com>
2018-12-29 19:25:50 +0200

    dl.sh

--- dl.sh
+++ dl.sh
@@ -5,6 +5,7 @@ set -e
 dl_into_current_dir() {
     wget https://github.com/xonixx/docker-elk/archive/master.zip
     unzip master.zip
+    rm ./docker-elk-master/es_data/.store_elastic_data_here
     cp -r docker-elk-master/* .
     rm -r master.zip docker-elk-master
 }
